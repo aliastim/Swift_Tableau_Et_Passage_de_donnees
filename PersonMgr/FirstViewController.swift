5ff06bf0a61f7964a10960844de37cc394f41f26
aliastim <33629825+aliastim@users.noreply.github.com>
2018-10-24 09:24:07 +0200

    Initial Commit

---
+++ PersonMgr/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PersonMgr
+//
+//  Created by Timothée CORRADO on 24/10/2018.
+//  Copyright © 2018 Timothée CORRADO. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PersonMgr/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PersonMgr
+//
+//  Created by Timothée CORRADO on 24/10/2018.
+//  Copyright © 2018 Timothée CORRADO. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
